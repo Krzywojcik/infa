339a062ac17639991fef66d5b79f492db8780272
Krzywojcik <krzywojcik@student.agh.edu.pl>
2024-10-15 17:34:24 +0200

    Update ŹródłoR2l.cpp

--- ŹródłoR2l.cpp
+++ ŹródłoR2l.cpp
@@ -2,31 +2,30 @@
 using namespace std;
 
 int main() {
-	int a; int b; int c; int d; int e; int max;
-	cout << "Podaj pierwsza liczbe" << endl;
-	cin >> a;
-	cout << "Podaj druga liczbe" << endl;
-	cin >> b;
-	cout << "Podaj trzecia liczbe" << endl;
-	cin >> c;
-	cout << "Podaj czwarta liczbe" << endl;
-	cin >> d;
-	cout << "Podaj piata liczbe" << endl;
-	cin >> e;
-	
-	if (a < b) {
-		max = b;
-	}
-	else if (a < c) {
-		max = c;
-	}
-	else if (a < d) {
-		max = d;
-	}
-	else if (a < e) {
-		max = e;
-	}
-	else max = a;
-	cout << "najwieksza liczba to " << i << endl;
+	int liczba1; int liczba2; int min_liczba; int max_liczba; int i;int roznica;
+	cout << "Podaj liczby" << endl;
+	cin >> liczba1; 
+
+	liczba1 = min_liczba;
+	liczba1 = max_liczba;
+
+     cin >> liczba2;
+
+for (i=0;i<5;i++)
+
+  if (liczba2 > liczba1){
+
+	liczba2 = max_liczba;
+}
+  if (liczba2 < liczba1){
+
+    liczba2 = min_liczba;
+}
+
+roznica = max_liczba - min_liczba;
+cout << "najmniejsza liczba to " << min_liczba << endl;
+cout << "najwieksza liczba to " << max_liczba << endl;
+cout << "roznica liczb to " << roznica << endl;
+
 	return 0;
 }
