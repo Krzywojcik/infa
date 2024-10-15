8db9012e2e7d3525081616d237cf27000b644706
Krzywojcik <krzywojcik@student.agh.edu.pl>
2024-10-15 18:31:54 +0200

    Update ŹródłoR2l.cpp

--- ŹródłoR2l.cpp
+++ ŹródłoR2l.cpp
@@ -2,30 +2,34 @@
 using namespace std;
 
 int main() {
-	int liczba1; int liczba2; int min_liczba; int max_liczba; int i;int roznica;
-	cout << "Podaj liczby" << endl;
-	cin >> liczba1; 
+	int liczba; int min_liczba; int max_liczba; int i; int roznica;
 
-	liczba1 = min_liczba;
-	liczba1 = max_liczba;
+	cout << "Podaj liczby" << endl; // ustawiamy pierwszą wpisaną liczbe jako max i min
+	cin >> liczba;
 
-     cin >> liczba2;
+	min_liczba = liczba;
+	max_liczba = liczba;
 
-for (i=0;i<5;i++)
 
-  if (liczba2 > liczba1){
+	for (i = 0; i < 4; i++) { // 4-krotnie wpisujemy liczby 
 
-	liczba2 = max_liczba;
-}
-  if (liczba2 < liczba1){
+		cin >> liczba;
 
-    liczba2 = min_liczba;
-}
+		if (liczba > max_liczba) { // sprawdzamy czy wpisana liczba jest większa od poprzednio podanej. 
 
-roznica = max_liczba - min_liczba;
-cout << "najmniejsza liczba to " << min_liczba << endl;
-cout << "najwieksza liczba to " << max_liczba << endl;
-cout << "roznica liczb to " << roznica << endl;
+			max_liczba = liczba; // Jeżeli tak to ustawaimy ją jako max
+		}
+		if (liczba < min_liczba) { // sprawdzamy czy wpisana liczba jest mniejsza od poprzednio podanej.
+
+			min_liczba = liczba; // Jeżeli tak to ustawaimy ją jako min
+		}
+	}
+	
+        roznica = max_liczba - min_liczba;
+	
+	cout << "najmniejsza liczba to " << min_liczba << endl; // wypisujemy wyniki
+	cout << "najwieksza liczba to " << max_liczba << endl;
+	cout << "roznica liczb to " << roznica << endl;
 
 	return 0;
 }
