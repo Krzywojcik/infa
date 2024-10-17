6ed11c20b14fe3b39554253f31315afffbf58939
Krzywojcik <krzywojcik@student.agh.edu.pl>
2024-10-17 23:52:36 +0200

    kurczaki.cpp

--- kurczaki.cpp
+++ kurczaki.cpp
@@ -5,20 +5,27 @@ using namespace std;
 int nogi(int kurczaki, int krowy, int swinie) {
 
 	return (kurczaki * 2 + krowy * 4 + swinie * 4);
-
 }
 
 int main() {
-	
+
 	int kurczaki;
 	int krowy;
 	int swinie;
-	
-	cout << "Podaj liczbe kurczakow,krow,swini" << endl;
 
-	cin >> kurczaki, krowy, swinie;
+	cout << "Podaj liczbe kurczakow" << endl;
 
-	cout << "Jest " << nogi(kurczaki krowy swinie) << "nóg" << endl;
+	cin >> kurczaki;
+
+	cout << "Podaj liczbe krow" << endl;
+
+	cin >> krowy;
+
+	cout << "Podaj liczbe swini" << endl;
+
+	cin >> swinie;
+
+	cout << "Ilosc nog wynosi " << nogi(kurczaki, krowy, swinie) << endl;
 
 	return 0;
 
