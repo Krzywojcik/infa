7fe84b910cc185dedb3e0ad2ca82e8c4a069643d
Krzywojcik <krzywojcik@student.agh.edu.pl>
2024-10-24 14:34:27 +0200

    Add files via upload

---
+++ Pitagorejskie.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int a;
+	int b;
+	int c;
+
+
+
+	cout << "Podaj peirwsza liczbe" << endl;
+	
+	cin >> a;
+
+	cout << "Podaj peirwsza liczbe" << endl;
+
+	cin >> b;
+
+	cout << "Podaj peirwsza liczbe" << endl;
+
+	cin >> c;
+
+	
+	int LiczbyPitagorejskie() {
+
+		int a;
+		int b;
+		int c;
+
+		if (a * a + b * b == c * c) {
+			return 1;
+
+		}
+
+	}
+	return 0;
+}
