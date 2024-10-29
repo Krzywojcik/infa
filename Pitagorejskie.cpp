e2e68639ee4cc491bb1ccc1800367e0b59f45ec1
Krzywojcik <krzywojcik@student.agh.edu.pl>
2024-10-29 20:37:53 +0100

    Update Pitagorejskie.cpp

--- Pitagorejskie.cpp
+++ Pitagorejskie.cpp
@@ -8,27 +8,34 @@ int LiczbyPitagorejskie(int a, int b, int c) {
 	else if (a * a + c * c == b * b) {
 		return 1;
 	}
-	else if 
-	}
-	}
+	else if (b * b + c * c == a * a){
+		return 1;
 }
+	else {
+		return 0;
+	}
+	}
+
 
 int main() {
 	int a;
 	int b;
 	int c;
 
-	cout << "Podaj peirwsza liczbe" << endl;
-	
+	cout << "Podaj pierwsza liczbe" << endl;
+
 	cin >> a;
 
-	cout << "Podaj peirwsza liczbe" << endl;
+	cout << "Podaj druga liczbe" << endl;
 
 	cin >> b;
 
-	cout << "Podaj peirwsza liczbe" << endl;
+	cout << "Podaj trzecia liczbe" << endl;
 
 	cin >> c;
+	
+	cout << "\n" << endl;
+	cout << LiczbyPitagorejskie(a, b, c) << endl;
 
 
 	return 0;
